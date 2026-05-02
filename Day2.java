62b7613aaa3965ea9de795acf3fb2d291d7c208f
pavanbarpete579-cloud <pavanbarpete579@gmail.com>
2026-05-02 08:53:57 +0530

    Implement methods for even/odd, largest, and grading

--- Day2.java
+++ Day2.java
@@ -1,1 +1,43 @@
 
+class Day2 {
+
+    // Problem 1
+    static void evenOdd(int num) {
+        if (num % 2 == 0)
+            System.out.println(num + " is Even");
+        else
+            System.out.println(num + " is Odd");
+    }
+
+    // Problem 2
+    static void largest(int a, int b, int c) {
+        if (a == b && b == c)
+            System.out.println("All are equal");
+        else if (a >= b && a >= c)
+            System.out.println("A is largest");
+        else if (b >= c)
+            System.out.println("B is largest");
+        else
+            System.out.println("C is largest");
+    }
+
+    // Problem 3
+    static void grade(int marks) {
+        if (marks >= 90)
+            System.out.println("A");
+        else if (marks >= 75)
+            System.out.println("B");
+        else if (marks >= 50)
+            System.out.println("C");
+        else
+            System.out.println("Fail");
+    }
+
+    // MAIN
+    public static void main(String[] args) {
+
+        evenOdd(10);       // Problem 1
+        largest(5,5,5);    // Problem 2
+        grade(72);         // Problem 3
+    }
+}
