c2b23e41e0de982cbd1cd83ab2e0f2fb9485a14b
pavanbarpete579-cloud <pavanbarpete579@gmail.com>
2026-05-02 09:25:36 +0530

    Implement inheritance with Person and Student classes

--- Inheritance.java
+++ Inheritance.java
@@ -1,1 +1,29 @@
 
+class Person {
+    String name;
+
+    Person(String name) {
+        this.name = name;
+    }
+}
+
+class Student extends Person {
+    int roll;
+
+    Student(String name, int roll) {
+        super(name);
+        this.roll = roll;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + ", Roll: " + roll);
+    }
+}
+
+public class Day5Inheritance {
+    public static void main(String[] args) {
+
+        Student s = new Student("Pavan", 101);
+        s.display();
+    }
+}
