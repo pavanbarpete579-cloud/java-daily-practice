30ffe5492c79fb4e079b7776bdd1f5eb2b2a32f1
pavanbarpete579-cloud <pavanbarpete579@gmail.com>
2026-05-02 08:58:36 +0530

    Implement pattern printing methods in Day3 class
    Added methods to print various patterns using asterisks and numbers.

--- Day3.java
+++ Day3.java
@@ -1,1 +1,55 @@
 
+class Day3 {
+
+    static void pattern1() {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern2() {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern3() {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pyramid() {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        pattern1();
+        System.out.println();
+
+        pattern2();
+        System.out.println();
+
+        pattern3();
+        System.out.println();
+
+        pyramid();
+    }
+}
