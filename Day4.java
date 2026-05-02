6bfd34151f47b00a9f6751675d4daa7ec0c3965d
pavanbarpete579-cloud <pavanbarpete579@gmail.com>
2026-05-02 09:09:31 +0530

    Add Student class and main method for display

--- Day4.java
+++ Day4.java
@@ -1,1 +1,24 @@
+class Student {
+    int id;
+    String name;
 
+    Student(int i, String n) {
+        id = i;
+        name = n;
+    }
+
+    void display() {
+        System.out.println(id + " " + name);
+    }
+}
+
+class Day4 {
+    public static void main(String[] args) {
+
+        Student s1 = new Student(101, "Pavan");
+        Student s2 = new Student(102, "Rahul");
+
+        s1.display();
+        s2.display();
+    }
+}
