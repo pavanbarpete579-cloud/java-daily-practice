951ad6e1aa14c1f045c16160350bb1b61297ebbe
pavanbarpete579-cloud <pavanbarpete579@gmail.com>
2026-05-02 09:19:46 +0530

    Add Student class with constructor and display method

--- ThisKeywordDemo.java
+++ ThisKeywordDemo.java
@@ -1,1 +1,25 @@
+class Student {
+    int id;
+    String name;
 
+    // Constructor using 'this'
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    void display() {
+        System.out.println("ID: " + id + ", Name: " + name);
+    }
+}
+
+public class ThisKeywordDemo {
+    public static void main(String[] args) {
+
+        Student s1 = new Student(101, "Pavan");
+        Student s2 = new Student(102, "Rahul");
+
+        s1.display();
+        s2.display();
+    }
+}
