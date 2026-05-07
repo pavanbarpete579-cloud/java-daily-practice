d35c1ea7249181ebe746621bd7d57791e25fe700
pavanbarpete579-cloud <pavanbarpete579@gmail.com>
2026-05-07 19:34:34 +0530

    Implement binary search in sorted array
    Implemented a binary search algorithm and added methods to print the array and handle user input.

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,1 +1,75 @@
+import java.util.Scanner;
 
+public class BinarySearch {
+
+    // Method for binary search
+    static int binarySearch(int arr[], int key) {
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == key) {
+                return mid;
+            }
+
+            else if (arr[mid] < key) {
+                low = mid + 1;
+            }
+
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    // Method to print array
+    static void printArray(int arr[]) {
+
+        System.out.println("Array Elements:");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+        System.out.println("Enter sorted array elements:");
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        printArray(arr);
+
+        System.out.print("Enter element to search: ");
+        int key = sc.nextInt();
+
+        int result = binarySearch(arr, key);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        }
+
+        else {
+            System.out.println("Element not found");
+        }
+
+        sc.close();
+    }
+}
