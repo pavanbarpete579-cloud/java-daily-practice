fb4fe78780832b582304227ccd2d5695852ff44c
pavanbarpete579-cloud <pavanbarpete579@gmail.com>
2026-05-07 19:37:05 +0530

    Implement bubble sort algorithm

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,1 +1,58 @@
+import java.util.Scanner;
 
+public class BubbleSort {
+  
+    static void bubbleSort(int arr[]) {
+
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = 0; j < n - i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+  
+    static void printArray(int arr[]) {
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Before Sorting:");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.println("After Sorting:");
+        printArray(arr);
+
+        sc.close();
+    }
+}
