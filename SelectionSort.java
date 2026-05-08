1e6b1da3c8a4bc34907789fb7034969850728556
pavanbarpete579-cloud <pavanbarpete579@gmail.com>
2026-05-08 17:18:59 +0530

    Implement selection sort algorithm
    Implemented selection sort algorithm with user input for array size and elements.

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,1 +1,60 @@
+import java.util.Scanner;
 
+public class SelectionSort {
+
+    static void selectionSort(int arr[]) {
+
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    static void printArray(int arr[]) {
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Before Sorting:");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.println("After Sorting:");
+        printArray(arr);
+
+        sc.close();
+    }
+}
